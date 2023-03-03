154738e2b24e7f8908fc99e7d13dfa402c046fc0
jordi <jordi.mallafre@hotmail.com>
2023-03-03 10:38:09 +0100

    Creació component menu

---
+++ src/components/llistatMenu.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "../css/styleLlistatMenu.css";
+
+function llistatMenu() {
+  return (
+    <ul className="menu">
+      <CreacioContingutInventari />
+    </ul>
+  );
+}
+
+function CreacioContingutInventari() {
+  function handleClick(a) {
+    const active = document.getElementsByClassName("active")[0];
+    if (active) active.classList.remove("active");
+    a.classList.add("active");
+  }
+  return (
+    <div className="BG-black"> 
+      <li>
+        <div>
+          <span className="span">Inventari</span>
+          <p className="p">Gestió d'inventari</p>
+        </div>
+      </li>
+      <a
+        href="http://localhost:3000/"
+        className="a"
+        role="button"
+        onClick={(event) => handleClick(event.currentTarget)}
+      >
+        <div className="divEnllaç">
+          <span className="spanSub">Categories</span>
+        </div>
+      </a>
+      <a
+        href="http://localhost:3000/"
+        className="a"
+        role="button"
+        onClick={(event) => handleClick(event.currentTarget)}
+      >
+        <div className="divEnllaç">
+          <span className="spanSub">Subcategories</span>
+        </div>
+      </a><a
+        href="http://localhost:3000/"
+        className="a"
+        role="button"
+        onClick={(event) => handleClick(event.currentTarget)}
+      >
+        <div className="divEnllaç">
+          <span className="spanSub">Materials</span>
+        </div>
+      </a><a
+        href="http://localhost:3000/"
+        className="a"
+        role="button"
+        onClick={(event) => handleClick(event.currentTarget)}
+      >
+        <div className="divEnllaç">
+          <span className="spanSub">Exemplars</span>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default llistatMenu;
---
+++ src/containers/menuContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LlistatMenu from '../components/llistatMenu'
+
+function menuContainer() {
+  return (
+    <div>
+        <LlistatMenu />
+    </div>
+  )
+}
+
+export default menuContainer
