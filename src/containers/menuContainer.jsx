8ffb77eb3820b0022d87c5bd6bd4d47a7ced710a
jordi <jordi.mallafre@hotmail.com>
2023-04-26 09:25:15 +0200

    Actualitzacio

--- src/containers/menuContainer.jsx
+++ src/containers/menuContainer.jsx
@@ -5,6 +5,7 @@ import UserMenu from '../components/menuComponents/usuariMenu';
 import InventariMenu from '../components/menuComponents/inventariMenu';
 import GeneralMenu from '../components/menuComponents/generalMenu';
 import ReservesMenu from '../components/menuComponents/reservesMenu';
+import LogoFinal from '../components/menuComponents/logoFinal';
 
 
 import "../css/styleImage.css";
@@ -25,8 +26,8 @@ function menuContainer() {
           <GeneralMenu />
           <ReservesMenu />
         </ul>
+        <LogoFinal />
       </div>
-
     </div>
   )
 }
